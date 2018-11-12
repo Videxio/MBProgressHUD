4da0f3a3e524aa7593b53524c3592467cc75e85c
Daniel Lazarenko <daniel.lazarenko@videxio.com>
2018-11-12 13:06:21 +0100

    fix precision loss warning

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -755,7 +755,7 @@ static const CGFloat MBDefaultDetailsLabelFontSize = 12.f;
 }
 
 - (void)updateProgressFromProgressObject {
-    self.progress = self.progressObject.fractionCompleted;
+    self.progress = (float)self.progressObject.fractionCompleted;
 }
 
 #pragma mark - Notifications
